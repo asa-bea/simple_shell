bbd06af0bd7bf498dca17b3a5b557a7c3c3df7d5
lyza08 <oppongliza18@gmail.com>
2023-06-20 14:03:26 -0700

    Betty would be proud

---
+++ add.c
@@ -0,0 +1,21 @@
+/* Authors: Asabea  and Liza */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "shell.h"
+
+/**
+ * add_numbers - Adds two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The sum of the two numbers
+ */
+int add_numbers(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
