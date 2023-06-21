d8d61fdce78ee9e22f9bbee2d2e116aac41cbfde
lyza08 <oppongliza18@gmail.com>
2023-06-21 02:49:53 -0700

    Task 1 debug

--- shell.c
+++ shell.c
@@ -1,37 +1,40 @@
 #include "shell.h"
+#include <stdio.h>
 
-/** buffer: A pointer to a character that will be used to store the user input.
-   * @buffer_size: The size of the buffer allocated for user input.
-   * @input_length: A signed integer to store the length of the user input.
-   * @child_pid: A process ID to store the child process ID.
-   * @status: An integer to store the exit status or termination signal of the
-   * child process
-   *
-   * return : 0 (successful)
-*/
-
-
+/**
+ * main - Entry point of the program
+ * @buffer_size: The size of the buffer allocated for user input.
+ * @input_length: A signed integer to store the length of the user input.
+ * @child_pid: A process ID to store the child process ID.
+ * @status: An integer to store the exit status or termination signal of the
+ *          child process
+ * Return: 0 on success
+ */
 int main(void)
 {
-   	char *buffer = NULL;
+	char *buffer = NULL;
 	size_t buffer_size = 0;
 	ssize_t input_length;
+
 	while (1)
-    {
-    	write(STDOUT_FILENO, "$ ", 2);  /* Displays the prompt */
+	{
+	write(STDOUT_FILENO, "$ ", 2); /* Displays the prompt */
 
-    	input_length = getline(&buffer, &buffer_size, stdin);
+	input_length = getline(&buffer, &buffer_size, stdin);
 
-    	if (input_length == -1) {
-        	write(STDOUT_FILENO, "\n", 1); /* Handle EOF condition*/
-        	break;
-    	}
+	if (input_length == -1)
+	{
+		write(STDOUT_FILENO, "\n", 1); /* Handle EOF condition */
+		break;
+	}
 
-    	buffer[input_length - 1] = '\0';  /* Remove trailing newline char*/
-
+	buffer[input_length - 1] = '\0'; /* Remove trailing newline char */
 
 	free(buffer);
 
 	return (0);
+	}
+
+	return (0);
 }
-}
+
