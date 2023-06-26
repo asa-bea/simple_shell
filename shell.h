ff139e65af22a513291b45f442ecf09083f8a02b
lyza08 <oppongliza18@gmail.com>
2023-06-26 08:14:28 -0700

    New files

--- shell.h
+++ shell.h
@@ -14,12 +14,29 @@ char** parse_arguments(char* command);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 
 int execute_command(char *buffer);
+int execute_command(char **args);
 
 void executeCommand(char *command);
 
 void display_error();
 void wait_for_child(pid_t pid, int* status);
 
+void my_function(void);
+
+char *read_line(void);
+char **split_line(char *line);
+void execute(char **args);
+int main(void);
+
+char *read_line(void);
+char **split_line(char *line);
+void execute(char **args);
+void shell_exit(char **args);
+
+int setenv(const char *variable, const char *value, int overwrite);
+int unsetenv(const char *variable);
+
+int main(int argc, char *argv[]);
 
 #define BUFFER_SIZE 1024
 
